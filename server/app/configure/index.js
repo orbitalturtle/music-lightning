08f380ce58db912e3f81b94f6dc99cb2a08b8c27
orbitalturtle <orbitalturtle@gmail.com>
2017-12-09 21:21:06 -0500

    Add file upload functionality

--- server/app/configure/index.js
+++ server/app/configure/index.js
@@ -1,4 +1,6 @@
 'use strict';
+var express = require('express');
+
 module.exports = function (app) {
 
     // setValue and getValue are merely alias
@@ -17,6 +19,11 @@ module.exports = function (app) {
     // Logging middleware, set as application
     // variable inside of server/app/configure/app-variables.js
     app.use(app.getValue('log'));
+
+    // So that server can handle passing large data
+    var bodyParser = require('body-parser');
+    app.use(bodyParser.json({limit: '50mb'}));
+    app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
     require('./authentication')(app);
 
