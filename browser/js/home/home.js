3b4fb291e5b806e34055538e28531ac05b8be253
orbitalturtle <orbitalturtle@gmail.com>
2018-05-03 21:35:39 -0400

    Get songs showing up on the home page

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,6 +1,33 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
+        controller: 'HomeController',
         templateUrl: 'js/home/home.html'
     });
 });
+
+app.controller('HomeController', function ($scope, HomeFactory) {
+
+    var songPromise = HomeFactory.getSongs();
+    
+    songPromise.then(function(response) {
+        $scope.songs = response; 
+
+    })
+
+});
+
+app.factory('HomeFactory', function ($http) {
+    var HomeFactory = {};
+
+    HomeFactory.getSongs = function() {
+        return $http.get('/api/songs')
+        .then(function(response) {
+            return response.data;
+        })
+    }
+
+    return HomeFactory;
+
+});
+
