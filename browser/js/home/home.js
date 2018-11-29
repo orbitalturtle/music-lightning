e9d7d19b1211002f8204734157773d6c9caded7e
orbitalturtle <orbitalturtle@gmail.com>
2018-11-28 22:52:24 -0500

    Play music files on the front page

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -7,18 +7,37 @@ app.config(function ($stateProvider) {
 });
 
 app.controller('HomeController', function ($scope, HomeFactory) {
-
     var songPromise = HomeFactory.getSongs();
     
     songPromise.then(function(response) {
         $scope.songs = response; 
         
-        // This is sort of an experimental thing I'm doing here ;-/ 
         for (var i = 0; i < $scope.songs.length; i++) {
-           // $scope.songs[i].song_art = Buffer.from($scope.songs.song_art.data).toString('base64'); 
-           $scope.songs[i].song_art = btoa(String.fromCharCode.apply(null, new Uint8Array($scope.songs[i].song_art.data)))
+           $scope.songs[i].song_art = btoa(String.fromCharCode.apply(null, new Uint8Array($scope.songs[i].song_art.data)));
+
+           var base64File = btoa(arrayBufferToString($scope.songs[i].song_file.data));
+
+           $scope.songs[i].song_file = base64File;
         }
     })
+
+    function arrayBufferToString(buffer){
+
+        var bufView = new Uint16Array(buffer);
+        var length = bufView.length;
+        var result = '';
+        var addition = Math.pow(2,16)-1;
+
+        for(var i = 0;i<length;i+=addition){
+
+            if(i + addition > length){
+                addition = length - i;
+            }
+            result += String.fromCharCode.apply(null, bufView.subarray(i,i+addition));
+        }
+
+        return result;
+    }
 
 });
 
